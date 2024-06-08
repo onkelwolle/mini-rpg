ae060eb5785f7b8228fdb42a921ee085f29f0e27
Wolfgang Zimmermann <wolfgang@zimmermann.red>
2024-06-08 15:58:37 +0200

    feat: use pygame lib instead of console output

--- game.py
+++ game.py
@@ -1,38 +1,20 @@
 import sys
-import time
+import pygame
 
 class Game:
     def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1280, 720))
+        self.clock = pygame.time.Clock()
         self.running = True
-        self.map = self.load_map("map.txt")
     
-    def load_map(self, filename):
-        with open(filename, 'r') as file:
-            return [list(line.strip()) for line in file]
-
     def run(self):
         while self.running:
-            self.clear_screen()
-            self.render()
-            self.handle_input()
-            self.update()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
 
-    def clear_screen(self):
-        sys.stdout.write('\033[2J\033[H')
-        sys.stdout.flush()
+            self.screen.fill("purple")
+            pygame.display.flip()
+            self.clock.tick(60)
 
-    def handle_input(self):
-        command = input("Enter command: (w/a/s/d/talk/quit): ")
-        if command in ['w', 'a', 's', 'd']:
-            print("not implemented yet")
-        elif command == 'talk':
-            print("not implemented yet")
-        elif command == 'quit':
-            self.running = False
-
-    def update(self):
-        pass
-
-    def render(self):
-        for row in self.map:
-            print(''.join(row))
